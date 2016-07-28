bf8d82084699680a992163edbc29a265bdac863c
18810848572 <18810848572@163.com>
2016-07-28 15:29:12 +0800

    主界面和用户请求过滤

---
+++ src/com/java1234/web/MainServlet.java
@@ -0,0 +1,26 @@
+package com.java1234.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.doPost(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		request.setAttribute("mainPage", "diary/diaryList.jsp");
+		request.getRequestDispatcher("mainTemp.jsp").forward(request, response);
+	}
+	
+}
